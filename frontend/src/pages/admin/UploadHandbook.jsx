cf80f14b93899de6005d117ce819ec6538c7691d
Fatima Yousra <fatimasanjinakarim@graduate.utm.my>
2026-06-22 11:22:26 +0800

    scaffold UploadHandbook component (UC031)

---
+++ frontend/src/pages/admin/UploadHandbook.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function UploadHandbook() {
+  return (
+    <div>
+      <h1>Upload Handbook</h1>
+    </div>
+  );
+}
+
+export default UploadHandbook;
+
