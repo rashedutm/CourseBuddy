e048c1a45bcdd7b8968e1927ee70a65f05bf147f
Fatima Yousra <fatimasanjinakarim@graduate.utm.my>
2026-07-04 22:49:57 +0800

    added top comment describing this module

--- frontend/src/pages/admin/UploadHandbook.jsx
+++ frontend/src/pages/admin/UploadHandbook.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { uploadHandbook } from "../../services/handbookService";
 import "./admin.css";
 
+// UC031: Upload Handbook
+// Lets an admin pick an intake + upload an Excel
+// handbook file.
+
 function UploadHandbook() {
 }
 return ()
