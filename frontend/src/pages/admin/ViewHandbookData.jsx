0fd7e022ef7fcd85d58d6faf609fde7a3d57f6c0
Fatima Yousra <fatimasanjinakarim@graduate.utm.my>
2026-07-04 23:05:16 +0800

    scaffolding View Handbook Data component UC032

---
+++ frontend/src/pages/admin/ViewHandbookData.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getAvailableYears,
+  getAvailableMonths,
+  getHandbookData
+} from "../../services/handbookService";
+import "./admin.css";
+
+export default function ViewHandbookData() {}
