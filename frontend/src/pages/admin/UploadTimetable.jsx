82a69c96e89526f7ec6b09e572e1c0d939890777
Fatima Yousra <fatimasanjinakarim@graduate.utm.my>
2026-07-04 23:33:31 +0800

    scaffold Upload Timetable component for admin to upload timetable Excel files

---
+++ frontend/src/pages/admin/UploadTimetable.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { uploadTimetable } from "../../services/timetableService";
+import "./admin.css";
+// UC034: Upload Timetable
+// Lets an admin upload timetable Excel file for a specific semester
+
+function UploadTimetable() {}
